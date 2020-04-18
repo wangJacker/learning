67406de12a8ad98de061b11c6ee6e1cab545fea3
wangye <wangjacker@163.com>
2020-04-18 15:16:26 +0800

    添加新项目

---
+++ my-projects/src/components/form/Kform.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div @click="handleClick">{{msg}}</div>
+    <span>{{foo}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    msg: {
+      type: String,
+      default: "456454566456"
+    }
+  },
+  data() {
+    return {
+      foo: "value"
+    };
+  },
+  methods: {
+    handleClick(e: any) {
+      this.$emit("handleClick", "aaaa", e);
+    }
+  }
+});
+</script>
+
+<style scoped>
+</style>
---
+++ my-projects/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Kform :msg="msg" ref="form" @handleClick="handleClick" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Kform from "@/components/form/Kform";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld,
+    Kform
+  },
+  data() {
+    return {
+      msg: "我是父组件传下来的值"
+    };
+  },
+  mounted() {
+    this.$refs.form.foo;
+    console.log((this.$refs.form.foo = "111"));
+    console.log((this.$children[0].foo = "455"));
+  },
+  methods: {
+    handleClick(e, a) {
+      console.log(e);
+      console.log(a);
+    }
+  }
+};
+</script>
